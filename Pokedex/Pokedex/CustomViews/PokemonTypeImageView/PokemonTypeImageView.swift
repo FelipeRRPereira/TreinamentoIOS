998855a4953762e9435b0d7d607903ee3f3ab801
Felipe Pereira <feliperrpereira@gmail.com>
2019-06-15 09:51:04 -0300

    Criando componentes.

--- Pokedex/Pokedex/CustomViews/PokemonTypeImageView/PokemonTypeImageView.swift
+++ Pokedex/Pokedex/CustomViews/PokemonTypeImageView/PokemonTypeImageView.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2019 CWI Software. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PokemonTypeImageView: UIImageView {
+    
+    let nibName = "PokemonTypeImageView"
+    
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var typeImageView: UIImageView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed(nibName, owner: self, options: nil)
+        contentView.fixInView(self)
+    }
+    
+    func config(type: PokemonType) {
+        self.typeImageView.image = type.icon
+    }
+    
+}
--- Pokedex/PokedexTests/PokedexTests.swift
+++ Pokedex/PokedexTests/PokedexTests.swift
@@ -19,15 +19,12 @@ class PokedexTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let jsonUrl = Bundle(for: PokedexTests.self).url(forResource: "pokemons", withExtension: "json")!
+        let data = try! Data(contentsOf: jsonUrl)
+//        print(String(data: data, encoding: .utf8))
+        
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    
 
 }
